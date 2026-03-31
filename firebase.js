98bf999166fc8c58a2217afe12d77396bae614bd
dg-oh <123929683+dg-oh@users.noreply.github.com>
2026-03-31 16:05:04 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,12 @@
 // firebase.js
 const firebaseConfig = {
-  apiKey: "여기에 입력",
-  authDomain: "여기에 입력",
-  projectId: "여기에 입력",
+  apiKey: "AIzaSyCmjuNJo7YvIewnPAZ2ATLQcb3iKgebSjM",
+    authDomain: "content-lab-site.firebaseapp.com",
+    projectId: "content-lab-site",
+    storageBucket: "content-lab-site.firebasestorage.app",
+    messagingSenderId: "345932911293",
+    appId: "1:345932911293:web:c0f158a04dcaf130285dd4",
+    measurementId: "G-KXYDHHYM9J"
 };
 
 firebase.initializeApp(firebaseConfig);
